c96c464695e3ea446bbed3205ecc45f6bc97f0f3
Matheus E. Muller <hello@memuller.com>
2017-04-10 07:16:18 -0300

    Service cmds runs using last service if none given.

--- lib/unix_utils.sh
+++ lib/unix_utils.sh
@@ -1,9 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
 # Runs service commands
 # First arg is the command, second is the service name (w/o .service)
-# $ service start mongodb
+# command may be e,d,s,p,r,l,t (enable,disable,start,stop,restart,reload,status)
+# if service is ommited or 'last' or 'l', will reuse service in previous call
+# $ s start mongodb
+# $ s restart
+# $ s t 
 function s() {
+  if [ $# -lt 2 ] || [ $2 = "l" ] || [ $2 = "last" ] ; then
+      srvc=$LAST_SERVICE
+  else
+    srvc=$2
+  fi
+  export LAST_SERVICE=$srvc
+
   case $1 in
     e)
       cmd="enable";;
@@ -22,7 +33,7 @@ function s() {
     *)
       cmd=$1
   esac
-  sudo systemctl $cmd $2.service
+  sudo systemctl $cmd $srvc.service
 }
 
 # Removes spaces on given input
@@ -36,7 +47,7 @@ function trim() {
 # third arg is the permission set for files.
 # $ perms . 755 644
 function perms(){
-  if [[ ! $# -eq 3 ]]; then
+  if [ ! $# -eq 3 ]; then
     echo "Call with three parameters (path, folderPermission, filePermissions)"
     return 1
   fi
