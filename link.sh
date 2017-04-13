e6651ea1999469204359437dcb2a53a4c258206a
Matheus E. Muller <hello@memuller.com>
2017-04-13 12:48:06 -0300

    Properly links ohmyzsh custom folder

--- link.sh
+++ link.sh
@@ -4,3 +4,4 @@ ln -s $(pwd)/gitconfig ~/.gitconfig
 ln -s $(pwd)/gitignore ~/.gitignore
 ln -s $(pwd)/atom ~/.atom
 ln -s $(pwd)/synergy/$(hostname) ~/.synergys
+ln -s $(pwd)/ohmyzsh ~/.oh-my-zsh/custom
