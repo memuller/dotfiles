ccf092529cda332521f82f4b448a289abf405325
Matheus E. Muller <hello@memuller.com>
2018-06-26 14:13:13 -0300

    Adds docker starter script for Cmder

--- cmder/user-profile.ps1
+++ cmder/user-profile.ps1
@@ -26,7 +26,12 @@
 function e. { explorer . }
 function env { ls variable:* }
 New-Alias g git
+
 New-Alias dc docker-compose
+function docker-start {
+    cd ~/Repos/env/dotfiles/docker ;
+    make run ;
+}
 
 # Docker Composer path fix for Windows, see
 # https://github.com/docker/compose/issues/4240
