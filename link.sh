6a91e8b6fc30bbbafc35574d033e7306a8269bf2
Matheus E. Muller <hello@memuller.com>
2018-06-28 23:07:37 -0300

    Links ~/local/share/applications folder on Gnome

--- link.sh
+++ link.sh
@@ -5,7 +5,13 @@ ln -s $(pwd)/gitignore ~/.gitignore
 ln -s $(pwd)/atom ~/.atom
 ln -s $(pwd)/synergy/$(hostname) ~/.synergys
 
+rm -fr ~/.zshrc ~/.oh-my-zsh/custom
 ln -s $(pwd)/ohmyzsh ~/.oh-my-zsh/custom
 ln -s $(pwd)/zshrc ~/.zshrc
 
 ln -s $(pwd)/xsendkeysrc ~/.xsendkeysrc
+
+if [ uname --all | grep -E '(Ubuntu|Fedora|Mint)' ]; then
+  rm -r ~/.local/share/applications
+  ln -s $(pwd)/local/share/applications ~/.local/share/applications
+fi
