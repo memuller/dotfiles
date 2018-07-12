6bfecf42f7f2955e037b03c06e93bbae90841fdd
Matheus E. Muller <hello@memuller.com>
2018-07-12 16:38:55 -0300

    Runs NVM on zshrc only if it's found

--- ohmyzsh/libs.zsh
+++ ohmyzsh/libs.zsh
@@ -27,5 +27,7 @@ if [ -d "$HOME/.rvm" ]; then
 fi
 
 # NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+if [ -d "$HOME/.nvm" ]; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+fi
