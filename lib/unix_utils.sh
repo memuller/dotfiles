34d79ac2700291c750d32d0acd3b0c9ff9c6c8bb
Matheus E. Muller <hello@memuller.com>
2018-10-12 00:38:00 +0000

    Adds docker cleanup script

--- lib/unix_utils.sh
+++ lib/unix_utils.sh
@@ -83,3 +83,9 @@ function keep() {
   mkdir -p $1
   touch $1/.gitkeep
 }
+
+function dockerClean() {
+  docker volume rm $(docker volume ls -qf dangling=true)
+  docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+  docker rm $(docker ps -qa --no-trunc --filter "status=exited")
+}
