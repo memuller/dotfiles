64f8c985b267baefb13a24e4147d4ee6256f2e5e
Matheus E. Muller <hello@memuller.com>
2019-01-27 18:41:34 -0200

    Changes VSCode.OSS icon

--- lib/flatpak.icons.fix.sh
+++ lib/flatpak.icons.fix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 FLATPACK_EXPORTS_APP=/var/lib/flatpak/exports/share/applications
-sudo sed -i -e 's/Icon=com.visualstudio.code/Icon=eclipse/g' $FLATPACK_EXPORTS_APP/com.visualstudio.code.desktop
+sudo sed -i -e 's/Icon=com.visualstudio.code.oss/Icon=eclipse/g' $FLATPACK_EXPORTS_APP/com.visualstudio.code.oss.desktop
 
 sudo sed -i -e 's/Icon=org.mozilla.FirefoxDevEdition/Icon=firefox/g' $FLATPACK_EXPORTS_APP/org.mozilla.FirefoxDevEdition.desktop
