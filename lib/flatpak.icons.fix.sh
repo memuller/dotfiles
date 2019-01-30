adb296dce1ac2461dfc4ef2028361ad489efe5cd
Matheus E. Muller <hello@memuller.com>
2019-01-30 11:44:07 -0200

    Updates icon-fixing script

--- lib/flatpak.icons.fix.sh
+++ lib/flatpak.icons.fix.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 FLATPACK_EXPORTS_APP=/var/lib/flatpak/exports/share/applications
+# VSCode.oss, flatpak'ed
 sudo sed -i -e 's/Icon=com.visualstudio.code.oss/Icon=eclipse/g' $FLATPACK_EXPORTS_APP/com.visualstudio.code.oss.desktop
-
+# Firefox Dev, flatpak'ed
 sudo sed -i -e 's/Icon=org.mozilla.FirefoxDevEdition/Icon=firefox/g' $FLATPACK_EXPORTS_APP/org.mozilla.FirefoxDevEdition.desktop
+# Slack, flatpak'ed
+sudo sed -i -e 's/Icon=com.slack.Slack/Icon=slack/g' $FLATPACK_EXPORTS_APP/com.slack.Slack.desktop
+# Discord, flatpak'ed
+sudo sed -i -e 's/Icon=com.discordapp.Discord/Icon=turpial/g' $FLATPACK_EXPORTS_APP/com.discordapp.Discord.desktop
+
+# Telegram
+sudo sed -i -e 's/Icon=telegram-desktop/Icon=telegram/g' /usr/share/applications/telegram-desktop.desktop
