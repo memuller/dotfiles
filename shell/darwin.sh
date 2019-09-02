ee613c219fd41167204968d6332663f5e078c7cd
Matheus E. Muller <hello@memuller.com>
2019-09-02 14:27:07 -0300

    fix(osx): handle autocomplete on multiuser brew

--- shell/darwin.sh
+++ shell/darwin.sh
@@ -11,5 +11,11 @@ alias wget="curl -O"
 # Loads brew'ed PHP
 [ -d "${BREW_PREFIX}/opt/php" ]  && export PATH="$(brew --prefix php)/bin/php:$PATH"
 
+# fixes issue w/ autocompletion on multiuser Homebrew 
+# happens because zcompinit refuses to run if unsafe dirs are on path but unsafe dirs (group permissions) are a common way to make brew work for multiple users
+if [ $BREW_PREFIX ]; then
+  alias zcompinit="zcompinit -u"
+fi
+
 # Starship theme
 eval "$(starship init zsh)"
