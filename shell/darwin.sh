72cb1dac67ca6d73c99fc157613144353f175e0e
Matheus E. Muller <hello@memuller.com>
2019-09-02 12:58:23 -0300

    feat(osx): loads brew'ed PHP

--- shell/darwin.sh
+++ shell/darwin.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Sets $BREW_PREFIX if Homebrew is installed
 command -v brew >/dev/null 2>/dev/null
 if [ $? = 0 ]; then
   export BREW_PREFIX="$(brew --prefix)"
 fi 
 
 alias wget="curl -O"
+
+# Loads brew'ed PHP
+[ -d "${BREW_PREFIX}/opt/php" ]  && export PATH="$(brew --prefix php)/bin/php:$PATH"
